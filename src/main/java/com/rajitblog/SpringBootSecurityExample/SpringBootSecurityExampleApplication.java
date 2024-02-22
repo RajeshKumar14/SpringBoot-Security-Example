56a1faad048b5a9d122d4a317d6fbd0bf571095a
Rajesh Kumar <kumarrajesh7671@gmail.com>
2024-02-22 23:42:07 +0530

    initial commit

---
+++ src/main/java/com/rajitblog/SpringBootSecurityExample/SpringBootSecurityExampleApplication.java
@@ -0,0 +1,14 @@
+package com.rajitblog.SpringBootSecurityExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityExampleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootSecurityExampleApplication.class, args);
+	}
+
+}
